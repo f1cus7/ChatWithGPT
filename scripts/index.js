dfe6285ecca4a16ef43f184126b861ad6d0cbe93
f1cus7 <f1cus7@mail.ru>
2025-06-07 09:56:38 +0300

    0.4 сделал анимацию чата

--- scripts/index.js
+++ scripts/index.js
@@ -19,21 +19,48 @@ setInterval(() => {
   document.getElementById("chat-time").textContent = `${hours}:${minutes}`;
 }, 1000);
 
+const btnExample = document.querySelector(".btn-send-example");
+const inputExample = document.querySelector(".input-example");
+const textExample = "Как ты высылаешь сообщения ботов?".split("");
 
-
-
-const btnExample = document.querySelector('.btn-send-example');
-const inputExample = document.querySelector('.input-example');
-const textExample = 'Как ты высылаешь сообщения ботов?'.split('');
-
-inputExample.addEventListener('input', () => {
+inputExample.addEventListener("input", () => {
   const valLength = inputExample.value.length;
   if (valLength <= textExample.length) {
-    inputExample.value = textExample.slice(0, valLength).join('');
+    inputExample.value = textExample.slice(0, valLength).join("");
   } else {
-    inputExample.value = textExample.join('');
+    inputExample.value = textExample.join("");
   }
 });
+
+btnExample.addEventListener("click", () => {
+  if (inputExample.value == textExample.join("")) {
+    inputExample.readOnly = true;
+    inputExample.value = '';
+    document.querySelector('.msg-to-bot').style.opacity = 1;
+    setTimeout(()=> {
+      document.querySelector('.msg-to-bot').style.marginBottom = '30vh';
+    },2000)
+    setTimeout(()=> {
+      document.querySelector('.msg-from-bot').style.opacity = 1;
+      new Typed("#json-typed", {
+    strings: [
+      `{
+    "name": "Ray",
+    "img": "ray.png",
+    "textId": 0,
+    "text": "Выпусти меня"
+}`
+    ],
+    typeSpeed: 30,
+    showCursor: false,
+    escapeHtml: true
+  });
+    },3000)
+      
+  }
+});
+
+
 
 // let data;
 
